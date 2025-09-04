0d6eefdc9486fe27a3b7dbae7ac1b28ce5d7cbd6
Dicky Eka Satria <dicky.eka99@gmail.com>
2025-09-04 18:07:44 +0000

    add env

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,5 +3,5 @@ import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
   plugins: [googleAI()],
-  model: 'googleai/gemini-2.0',
+  model: process.env.GOOGLE_GENAI_MODEL || 'googleai/gemini-2.0',
 });
